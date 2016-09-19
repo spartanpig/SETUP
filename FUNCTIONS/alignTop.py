3db9a72c216a12e505ebc26fd5aa747c469cca83
Lorem <spartanpig@gmail.com>
2016-09-19 11:09:51 -0700

    testing

---
+++ FUNCTIONS/alignTop.py
@@ -0,0 +1,11 @@
+import rhinoscriptsyntax as rs
+
+objs= rs.GetObjects('target',0,True, True)
+
+objs2= rs.GetObjects('conformers',0,True, True)
+
+bbox= rs.BoundingBox(objs)
+
+for obj in objs2:
+    bbox2= rs.BoundingBox(obj)
+    rs.MoveObject(obj, rs.VectorCreate([0,0,bbox[4][2]],[0,0,bbox2[4][2]]))
